9cd294b8e8d5d0d8313f2c506f0fe8b4f15e2a31
KHSCS091 <KHSCS091@KATYISD.ORG>
2018-10-25 10:12:57 -0500

    Began working on PlaceNumbers method in cell class

--- cell.java
+++ cell.java
@@ -8,136 +8,70 @@ import javafx.scene.image.ImageView;
 public class cell 
 {
 	ImageView i;
+	boolean isBomb;
 	int x;
 	int y;
 
-	public cell(ImageView k, int a, int b)
+	public cell()
 	{
-		i = k;
+		i = null;
+		isBomb = false;
+		x = 0;
+		y = 0;
+	}
+	
+	public cell(boolean isB, int a, int b, ImageView l)
+	{
+		i = l;
+		isBomb = isB;
 		x = a;
 		y = b;
 	}
 	
-	public cell(int a, int b)
+	public cell(boolean isB, int a, int b)
 	{
+		isBomb = isB;
 		x = a;
 		y = b;
 	}
 	
-	public static int[][] placeBombs()
+	public boolean equalTo(cell c)
 	{
-		int[][] bombs = new int[16][16];		
-		
-		for(int rows = 0;rows<16;rows++)
+		if(this.x == c.x && this.y == c.y)
 		{
-			if(rows % 2 == 0)
-			{
-				Random a = new Random();
-				Random b = new Random();
-					
-				int first = a.nextInt(16);
-				int second = b.nextInt(16);
-					
-				bombs[rows][first] = 1;
-				bombs[rows][second] = 1;
-			}
-			if(rows % 2 == 1)
-			{
-				Random a = new Random();
-				Random b = new Random();
-				Random c = new Random();
-				
-				int first = a.nextInt(16);
-				int second = b.nextInt(16);
-				int third = c.nextInt(16);
-				
-				bombs[rows][first] = 1;
-				bombs[rows][second] = 1;
-				bombs[rows][third] = 1;
-			}
+			return true;
 		}
-			
-			for(int rows = 0;rows<16;rows++)
-			{
-				for(int collums = 0;collums<16;collums++)
-				{
-					if(bombs[rows][collums] != 1)
-					{
-						bombs[rows][collums] = 0;
-					}
-				}
-			}
-		return bombs;
+		else
+		{
+			return false;
+		}
 	}
 	
-	public int[][] equalTo(int x, int y)
+	public static cell[][] placeNumbers(cell[][] g, int[][] b)
 	{
-		int[][] grid = new int[16][16];
-		grid = placeBombs(); 
 		for(int i = 0;i<16;i++)
 		{
 			for(int k = 0;k<16;k++)
 			{
-				if(grid[x][y] == grid[this.x][this.y])
+				if(b[i][k] == 0)
 				{
-					if(x >= 8)
-					{
-						Random ran = new Random();
-						int n = ran.nextInt(7);
-						grid[x][y] = grid[this.x][n];
-					}
-					else
-					{
-						Random ran = new Random();
-						int n = ran.nextInt((16 - 9) + 1) + 9;
-						grid[x][y] = grid[this.x][n];
-					}
+					
 				}
 			}
 		}
-		return grid;
 	}
 	
-	
-	
-	
+	public static int countBombs(cell[][] g, int[][]b, cell c, int count)
+	{
+		int count = 0;
+		if(b[c.x-1] [c.y+1] == 1)
+		{
+			count += 1;
+			
+		}
+		
+	}
 	
 	// 8 Cells to check 
-	
-	public static boolean checkforBombs(int[][] g, int x, int y, boolean c)
-	{
-		ArrayList<Boolean> results = new ArrayList<Boolean>();
-		
-		if(x == 0 && y == 0 || x == 0 && y == 15 || x == 15 && y == 0 || x == 15 && y == 15)
-		{
-			if(g[x][y] == 0)
-			{
-				
-			}
-			else
-			{
-				
-			}
-			for(int i = 0;i<8;i++)
-			{
-					
-			}
-		}
-		else
-		{
-			if(g[x][y] == 0)
-			{
-				results.add(false);
-			}
-			else
-			{
-				results.add(true);
-			}
-		
-			for()
-			{
-				
-			}
-		}
-	} 
+
 }
--- sweeperofMines.java
+++ sweeperofMines.java
@@ -12,6 +12,8 @@ import java.io.*;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+
+import java.util.ArrayList;
 import java.util.Random;
 import School.cell;
 
@@ -37,8 +39,10 @@ public class sweeperofMines extends Application
 		
 		Scanner sc = new Scanner("System.in");
 		GridPane grid = new GridPane();
+		cell[][] squareGrid = new cell[16][16];
 		
 		ImageView[][] gridButtons = new ImageView[16][16];
+		
 		
 		for(int i = 0;i<16;i++)
 		{
@@ -47,13 +51,15 @@ public class sweeperofMines extends Application
 				ImageView coveredView = new ImageView(covered);
 				ImageView emptyView = new ImageView(empty);
 				
-				coveredView.setOnMouseClicked((event)->
+				cell c = new cell(false, i, k, coveredView);
+				
+				c.i.setOnMouseClicked((event)->
 				{
-		            
+		            c.i.setImage(empty);
 		        });
 				
-				gridButtons[i][k] = coveredView;
-				grid.add(gridButtons[i][k], i, k);
+				squareGrid[i][k] = c;
+				grid.add(squareGrid[i][k].i, i, k);
 			}
 		}
 		
@@ -69,20 +75,13 @@ public class sweeperofMines extends Application
 		minesLeft.setVisible(true);
 		
 		int temp = 0;
-		int[][] bombs = new int[16][16];
-		bombs = placeBombs();
-		for(int i = 0;i<16;i++)
-		{
-			for(int k = 0;k<16;k++)
-			{
-				if(bombs[i][k] == 1)
-				{
-					temp+=1;
-				}
-			}
-		}
+		cell[] bombs = new cell[16];
 		
-		System.out.println(temp);
+//		bombs = placeBombs();
+		
+//		int[][] bombsInt = new int[16][16];
+		
+//		bombsInt = placeBombsInt(bombs);
 		
 		grid.setTranslateX((scene.getHeight()/2)/2-10);
 		grid.setTranslateY((scene.getWidth()/2)/2);
@@ -96,5 +95,93 @@ public class sweeperofMines extends Application
 	public static void main(String args[]) throws IOException
 	{
 		launch(args);
+	}
+	
+	public static cell[] placeBombs()
+	{
+		cell[] bombs = new cell[40];
+		ArrayList<cell> bombList = new <cell>ArrayList();
+		
+		Random startX = new Random();
+		Random startY = new Random();
+		
+		int x = startX.nextInt(16);
+		int y = startY.nextInt(16);
+		
+		cell start = new cell(true, x, y);
+		
+		bombList.add(start);
+		
+		while(bombList.size() < 40)
+		{
+			Random r1 = new Random();
+			Random r2 = new Random();
+			
+			int a = r1.nextInt(16);
+			int b = r2.nextInt(16);
+			
+			cell c = new cell(true, a, b);
+			
+			int count = 0;
+			
+			for(int i = 0;i<=bombList.size();i++)
+			{
+				if(c.equalTo(bombList.get(i)))
+				{
+					break;
+				}
+				
+				if(c.equalTo(bombList.get(i)) == false)
+				{
+					count += 1;
+				}
+				
+				if(count == bombList.size())
+				{
+					bombList.add(c);
+					break;
+				}
+			}
+		}
+		
+		for(int i = 0;i<40;i++)
+		{
+			bombs[i] = bombList.get(i);
+		}
+		
+		return bombs;
+	}
+	
+	public static int[][] placeBombsInt(cell[] c)
+	{
+		int[][] bombsInt = new int[16][16];
+		
+		for(int i = 0;i<40;i++)
+		{
+			if(c[i] != null)
+			{
+				bombsInt[c[i].x][c[i].y] = 1;
+			}
+			else
+			{
+				bombsInt[c[i].x][c[i].y] = 0;
+			}
+		}
+		
+		return bombsInt;
+	}
+	
+	public static boolean checkforBombs(cell[][] g, cell c, boolean firstClick)
+	{
+		ArrayList<Boolean> results = new ArrayList<Boolean>();
+		
+		int x = c.x;
+		int y = c.y;
+		
+		if(g[x][y].isBomb == false)
+		{
+			
+		}
+		
 	}
 }
